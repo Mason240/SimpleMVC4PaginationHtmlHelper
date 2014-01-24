ccfbc1d7cefd0ca1b8353a06ffd4ce461c57a307
Mason240 <mason240@gmail.com>
2014-01-24 13:08:45 -0600

    Initial Commit

---
+++ Pagination.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaginationService
+{
+
+    public class PagedList<T>
+    {
+        public int itemsPerPage { get; set; }
+        public int pageCount { get; set; }
+        public int currentPage { get; set; }
+        public List<T> pagedList { get; set; }
+
+        public PagedList(int itemsPerPage, int pageCount, int currentPage, List<T> pagedList)
+        {
+            this.itemsPerPage = itemsPerPage;
+            this.pageCount = pageCount;
+            this.currentPage = currentPage;
+            this.pagedList = pagedList;
+        }
+    }
+
+    public class PaginationService
+    {
+        private const int ITEMS_PER_PAGE = 15;
+
+        public int itemsPerPage { get; set; }
+
+        public PaginationService()
+        {
+            initialize(ITEMS_PER_PAGE);
+        }
+        public PaginationService(int itemsPerPage)
+        {
+            initialize(itemsPerPage);
+        }
+
+        private void initialize(int itemsPerPage)
+        {
+            this.itemsPerPage = itemsPerPage;
+        }
+
+        /// <summary>Parses the UrlRoutingString value from the URL for pagination. Default return is 1.</summary>
+        public int ParseUrlRoutingString(string UrlRoutingString)
+        {
+            if (string.IsNullOrWhiteSpace(UrlRoutingString)) return 1;
+
+            int pageNumber = 1;
+            bool parse = int.TryParse(UrlRoutingString, out pageNumber);
+            if (!parse || pageNumber < 1) return 1;
+
+            return pageNumber;
+        }
+
+        public PagedList<T> CreatePagedList<T>(List<T> itemList, int pageNumber)
+        {
+            if (pageNumber < 1) pageNumber = 1;
+            int itemListCount = itemList.Count;
+            int pageCount = (itemListCount / itemsPerPage) + 1;
+
+            //List of items will fit onto one page, return itemList
+            if (itemListCount <= itemsPerPage) return new PagedList<T>(itemsPerPage, 1, 1, itemList);
+
+            //Page number is higher than the number of possible pages, set pageNumber to the last page.
+            if (pageNumber > pageCount) pageNumber = pageCount;
+
+            int firstResult = (pageNumber - 1) * itemsPerPage;
+            itemList = itemList.Skip(firstResult).Take(itemsPerPage).ToList();
+
+            return new PagedList<T>(itemsPerPage, pageCount, pageNumber, itemList);
+        }
+    }
+}
---
+++ PaginationHtmlHelper.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PaginationService
+{
+    public static class PaginationHtmlHelper
+    {
+
+        private const int PAGES_DISPLAYED = 9;
+        private const string PAGE_URL_PARAMATER = "Page";
+
+        private const string CSS_CONTAINER = "pagination-container";
+        private const string CSS_CURRENT = "pagination-current";
+        private const string CSS_ELLIPSES = "pagination-ellipses";
+        private const string CSS_FIRST = "pagination-first";
+        private const string CSS_LAST = "pagination-last";
+
+
+        /// <summary>
+        /// Creates a div with links for pagination.</summary>
+        /// <param name="helper"></param>
+        /// <param name="currentPage">The current page number.</param>
+        /// <param name="totalPageCount">The total number of pages.</param>
+        /// <returns> div filled with links </returns>
+        public static MvcHtmlString PaginationHelper(this HtmlHelper helper, int currentPageNumber, int totalPageCount)
+        {
+            return PaginationHelper(helper, currentPageNumber, totalPageCount, PAGES_DISPLAYED, PAGE_URL_PARAMATER);
+        }
+
+        /// <summary>
+        /// Creates a div with links for pagination.
+        /// </summary>
+        /// <param name="helper"></param>
+        /// <param name="currentPage">The current page number.</param>
+        /// <param name="totalPageCount">The total number of pages.</param>
+        /// <param name="pagesDisplayed">The number of pages to be displayed in the middle section. Should be odd.</param>
+        /// <param name="pageUrlParamater">The name of the URL paramater representing the page number. Typically 'Page'</param>
+        /// <returns></returns>
+        public static MvcHtmlString PaginationHelper(this HtmlHelper helper, int currentPage, int totalPageCount, int pagesDisplayed, string pageUrlParamater)
+        {
+
+            if (totalPageCount == 1) return new MvcHtmlString(""); 
+            if (pagesDisplayed < 3) pagesDisplayed = 3; //Validate to make sure pagesDisplayed is greater than 3
+            string url = createURL(helper.ViewContext.HttpContext.Request.Url, pageUrlParamater);
+
+            StringBuilder builder = new StringBuilder();
+
+            //Open tag for containing div
+            builder.Append("<div class='" + CSS_CONTAINER + "'>");
+
+            //Create first link
+            builder.Append("<a href='" + url + "1' class='" + CSS_FIRST + " " + (currentPage == 1 ? CSS_CURRENT : "") + "' >First</a>"); 
+
+            //Starting and stopping values for the middle section of links
+            int startMiddle = (currentPage > (pagesDisplayed + 1) ? (currentPage - pagesDisplayed) : 2);
+            int stopMiddle = ((currentPage + pagesDisplayed) < totalPageCount ? (currentPage + pagesDisplayed) : (totalPageCount - 1));
+            //System.Diagnostics.Debug.WriteLine("--startMiddle " + startMiddle + " stopMiddle " + stopMiddle );
+
+            //Ellipeses
+            if ((startMiddle > 2) && (totalPageCount > 2)) builder.Append("<a class='" + CSS_ELLIPSES + "'> ... </a>"); 
+
+            //Build middle links
+            for (int i = startMiddle; i < stopMiddle+1; i++)
+            {
+                builder.Append("<a class='" + (currentPage == i ? CSS_CURRENT : "") + "' href=" + url + i + ">" + i + "</a>");
+            }
+
+            //Ellipeses
+            if ((stopMiddle != (totalPageCount - 1)) && (totalPageCount > 2)) builder.Append("<div style='float:left;margin-left:10px;'><b> ... </b></div>");
+
+            //Create last link
+            builder.Append("<a href='" + url + totalPageCount + "' class='" + CSS_LAST + " " + (currentPage == totalPageCount ? CSS_CURRENT : "") + "' >Last</a>");
+
+            //Closing tag for containing div
+            builder.Append("</div>");
+
+            return new MvcHtmlString(builder.ToString());
+        }
+
+
+        //Uses the URI from the current page to create the URL used for the paging links;
+        //The Page paramter is removed and then added back in empty, at the end of the string
+        //ie: http://localhost:55145/Ticket/TicketList?Owner=10955&Page=
+        //The Page value is then added at each step in PaginationHelper method 
+        private static string createURL(Uri urlObj, string pageUrlParamater)
+        {
+            //Get the query paramaters,removing the page paramater 
+            NameValueCollection nameValues = HttpUtility.ParseQueryString(urlObj.Query.ToString());
+            nameValues.Remove(pageUrlParamater);
+
+            //Get routing part of URI
+            string urlBase = urlObj.ToString().Split('?')[0];
+
+            //Combing and return
+            return urlBase + "?" + nameValues.ToString() + "&" + pageUrlParamater + "=";
+        }
+
+    }
+}
